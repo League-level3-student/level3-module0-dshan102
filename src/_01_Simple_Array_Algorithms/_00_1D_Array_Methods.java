29177c018c1241caf009ee7d7952ff795c0abd7c
dshan102 <53541391+dshan102@users.noreply.github.com>
2021-07-09 18:09:07 -0700

    Array_Methods (IP)

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -6,7 +6,10 @@ public class _00_1D_Array_Methods {
     //1. Complete the method so that it returns the sum of all
     //   of the integers in the array being passed in
     public static int sumIntArray(int[] values) {
-        return 0;
+    	for (int i=0; i<values.length; i++ ) {
+    		
+    	}
+        return -3;
     }
 
     //2. Complete the method so that it returns the average of all
